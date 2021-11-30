0bef571f4d312dc26e91391d732b70ac97d9b946
Shahsad Kolathur <shahsadkpklr@gmail.com>
2021-11-30 13:10:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,7 +8,7 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON
 from helper_func import subscribed, encode, decode, get_messages
-from database.sql import add_user, query_msg, full_userbase
+from database.mongo import present_in_userbase, add_to_userbase, get_users, del_from_userbase
 
 
 #=====================================================================================##
@@ -24,10 +24,11 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
     user_name = '@' + message.from_user.username if message.from_user.username else None
-    try:
-        await add_user(id, user_name)
-    except:
-        pass
+    if not await present_in_userbase(id):
+        try:
+            await add_to_userbase(id)
+        except:
+            pass
     text = message.text
     if len(text)>7:
         try:
@@ -146,13 +147,13 @@ async def not_joined(client: Client, message: Message):
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
-    users = await full_userbase()
-    await msg.edit(f"{len(users)} users are using this bot")
+    users = await get_users()
+    await msg.edit(f"{len(len(users))} users are using this bot")
 
 @Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
     if message.reply_to_message:
-        query = await query_msg()
+        query = await get_users()
         broadcast_msg = message.reply_to_message
         total = 0
         successful = 0
@@ -161,24 +162,35 @@ async def send_text(client: Bot, message: Message):
         unsuccessful = 0
         
         pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
-        for row in query:
-            chat_id = int(row[0])
+        for chat_id in query:
             try:
-                await broadcast_msg.copy(chat_id)
-                successful += 1
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-                await broadcast_msg.copy(chat_id)
+                await copy_message(broadcast_msg, chat_id)
                 successful += 1
             except UserIsBlocked:
+                await del_from_userbase(chat_id)
                 blocked += 1
             except InputUserDeactivated:
+                await del_from_userbase(chat_id)
                 deleted += 1
             except:
                 unsuccessful += 1
                 pass
             total += 1
-        
+            
+            try:
+                await pls_wait.edit(
+                    f"""<b><u>Broadcast Status</u>
+
+Total Users: <code>{total}</code>
+Successful: <code>{successful}</code>
+Blocked Users: <code>{blocked}</code>
+Deleted Accounts: <code>{deleted}</code>
+Unsuccessful: <code>{unsuccessful}</code></b>"""
+               )
+
+            except:
+                pass
+
         status = f"""<b><u>Broadcast Completed</u>
 
 Total Users: <code>{total}</code>
@@ -187,9 +199,20 @@ Blocked Users: <code>{blocked}</code>
 Deleted Accounts: <code>{deleted}</code>
 Unsuccessful: <code>{unsuccessful}</code></b>"""
         
-        return await pls_wait.edit(status)
+        try:
+            await pls_wait.edit(status)
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            await pls_wait.edit(status)
 
     else:
         msg = await message.reply(REPLY_ERROR)
         await asyncio.sleep(8)
         await msg.delete()
+
+async def copy_message(message, chat_id):
+    try:
+        await message.copy(chat_id)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        await copy_message(message, chat_id)
