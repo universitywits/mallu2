f25edc24d0ac61dc537c6a2b2e2389184596261c
Shahsad Kolathur <shahsadkpklr@gmail.com>
2021-11-30 18:37:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -145,7 +145,7 @@ async def not_joined(client: Client, message: Message):
     )
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
-async def get_users(client: Bot, message: Message):
+async def get_users_number(client: Bot, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
     users = await get_users()
     await msg.edit(f"{len(len(users))} users are using this bot")
