594185fd0e13712ff27ea24c61fd8ab86ad233af
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2021-12-18 12:36:38 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -105,7 +105,7 @@ async def start_command(client: Client, message: Message):
                 id = message.from_user.id
             ),
             reply_markup = reply_markup,
-            disable_web_page_preview = True,
+            #disable_web_page_preview = True,
             quote = True
         )
         return
@@ -141,7 +141,7 @@ async def not_joined(client: Client, message: Message):
             ),
         reply_markup = InlineKeyboardMarkup(buttons),
         quote = True,
-        disable_web_page_preview = True
+        #disable_web_page_preview = True
     )
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
